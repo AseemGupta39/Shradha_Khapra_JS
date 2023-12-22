7072ab61a5787596907618fc676b087e6bd0f571
Aseem Gupta <aseemguptajaipur@gmail.com>
2023-12-22 12:14:22 +0530

    tic tac toe almost pura ho gaya hai

--- script.js
+++ script.js
@@ -177,7 +177,7 @@
 // para.classList.remove('newclass');/
 
 
-let btn1 = document.querySelector('#btn1');
+// let btn1 = document.querySelector('#btn1');
 
 // btn1.onclick = (evt) => {
 //     console.log(evt);
@@ -218,10 +218,10 @@ let btn1 = document.querySelector('#btn1');
 //     console.log('inside div');
 // }
 
-let modeBtn = document.querySelector('#mode');
-let currMode = "light";
-let theme = "white";
-let body = document.querySelector('body');
+// let modeBtn = document.querySelector('#mode');
+// let currMode = "light";
+// let theme = "white";
+// let body = document.querySelector('body');
 
 // modeBtn.addEventListener('click',() => {
 //     modeBtn.innerText = `change to ${currMode} mode`
@@ -231,20 +231,103 @@ let body = document.querySelector('body');
 //     // console.log(`switching to ${currMode} mode`);
 // })
 
-modeBtn.addEventListener('click',() => {
-    modeBtn.innerText = `change to ${currMode} mode`
-    if(currMode === "light"){
-        currMode = "dark";
-        body.classList.remove("light");
-        body.classList.add("dark");
-    }
-    else{
-        currMode = "light";
-        body.classList.remove("dark");
-        body.classList.add("light");
-    }
-    console.log(currMode);
-    console.log(body.classList);
+// modeBtn.addEventListener('click',() => {
+//     modeBtn.innerText = `change to ${currMode} mode`
+//     if(currMode === "light"){
+//         currMode = "dark";
+//         body.classList.remove("light");
+//         body.classList.add("dark");
+//     }
+//     else{
+//         currMode = "light";
+//         body.classList.remove("dark");
+//         body.classList.add("light");
+//     }
+//     console.log(currMode);
+//     console.log(body.classList);
 
-    // console.log(`switching to ${currMode} mode`);
-})
+//     // console.log(`switching to ${currMode} mode`);
+// })
+
+
+let boxes = document.querySelectorAll('.box');
+let resetBtn = document.querySelector('#reset-btn');
+let msgContainer = document.querySelector('.msg-container');
+let msg = document.querySelector('#msg');
+
+
+let turnO = true;
+const winPatterns = [[0,1,2],
+                    [0,3,6],
+                    [0,4,8],
+                    [1,4,7],
+                    [2,5,8],
+                    [2,4,6],
+                    [3,4,5],
+                    [6,7,8]];
+
+
+
+boxes.forEach((box,i) => {
+box.addEventListener("click", () => {
+    console.log(`box was clicked ${i}`);
+    
+    box.innerText = turnO?'O':'X';
+    turnO = !turnO;
+    box.disabled = true;
+    checkWinner();
+
+});
+});
+
+// boxes.forEach((box) => {
+//     box.addEventListener(('click',() => {
+//         // console.log(`box was clicked ${i}`);
+//     }))
+// })
+
+
+const disableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+        // box.enableBoxes = true;
+    }
+}
+
+const showWinner = (winner) => {
+   msg.innerText = `winnner is ${winner}`;
+   msg.classList.remove("hide"); 
+   disableBoxes();
+} 
+
+const checkWinner = () => {
+    for(const pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;    
+        // console.log(pos1Val,pos2Val,pos3Val);
+
+        if(pos1Val!="" && pos2Val!="" && pos3Val!=""){
+            if(pos1Val=== pos2Val && pos2Val === pos3Val){
+                console.log('winner');
+                showWinner(pos1Val);
+
+            }
+        }
+    }
+};
+
+
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+}
+
